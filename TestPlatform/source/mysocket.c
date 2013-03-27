81d75abdd39f5681e864bc9bf929ca254a172f9f
Travis <art.of.war.sunzi@gmail.com>
2013-03-27 18:04:45 +0800

    upload

--- TestPlatform/source/mysocket.c
+++ TestPlatform/source/mysocket.c
@@ -3,21 +3,71 @@
 
 int create_server_to_listen(const short port, const Func func)
 {
-    WSADATA wsd;
-    SOCKET server_socket, cient_socket;
-    
+    WSADATA  Ws;
+    SOCKET ServerSocket, ClientSocket;
     struct sockaddr_in LocalAddr, ClientAddr;
-    if( WSAStartup(MAKEWORD(2,0),&wsd) )
-	{
-        //fprint("Init Windows Socket Failed");
-		return 1;
-	}
-    server_socket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
- 
-    if( cClient == INVALID_SOCKET)
-	{
-		return 0;
-	}
+    int Ret = 0;
+    int AddrLen = 0;
+    HANDLE hThread = NULL;
+
+    //Init Windows Socket
+    if ( WSAStartup(MAKEWORD(2,2), &Ws) != 0 )
+    {
+        printf("Init Windows Socket Failed:%s\n",GetLastError());
+        return -1;
+    }
+
+    //Create Socket
+    ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if ( ServerSocket == INVALID_SOCKET )
+    {
+        printf("Create Socket Failed:%s\n",GetLastError());
+        return -1;
+    }
+
+    LocalAddr.sin_family = AF_INET;
+    LocalAddr.sin_addr.s_addr = inet_addr(CONTROLER_PORT);
+    LocalAddr.sin_port = htons(port);
+    memset(LocalAddr.sin_zero, 0x00, 8);
+
+    //Bind Socket
+    Ret = bind(ServerSocket, (struct sockaddr*)&LocalAddr, sizeof(LocalAddr));
+    if ( Ret != 0 )
+    {
+        printf("Bind Socket Failed:%s\n",GetLastError());
+        return -1;
+    }
+
+    while(1)
+    {
+        Ret = listen(ServerSocket, 100);
+        if ( Ret != 0 )
+        {
+            printf("listen Socket Failed:%s\n",GetLastError());
+            return -1;
+        }
+
+        //muti-thread deal with the listen
+        
+        
+        while(1)
+        {
+            AddrLen = sizeof(ClientAddr);
+            ClientSocket = accept(ServerSocket,(struct sockaddr*)&ClientAddr,&AddrLen);
+            if (ClientSocket==INVALID_SOCKET)
+            {
+                printf("Accpet Failed:%s\n",GetLastError());
+                break;
+            }
+            // client info in ClientAddr, can get info
+            // address:inet_ntoa(ClientAddr.sin_addr) ;port: ClientAddr.sin_port
+
+            // create thread to deal with the socket
+        }
+        closesocket(ServerSocket);
+        closesocket(ClientSocket);
+    }
+    
     return 0;
 }
 
--- TestPlatform/source/mythread.h
+++ TestPlatform/source/mythread.h
@@ -4,8 +4,12 @@
 
 #ifndef __MY_THREAD__
 #define __MY_THREAD__
-
+#include "mysockect.h"
 #include <windows.h>
+
+
+int create_thread(const Func func, const char*[]);
+
 
 
 
